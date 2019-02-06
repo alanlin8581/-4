ded57c28f69aea9562162a73d33f5e9b2aa2524c
alanlin8581 <43751729+alanlin8581@users.noreply.github.com>
2019-02-06 10:06:18 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,22 @@
+$("button").click(function(){
+    $("button").text("双击");
+});
+
+$("button").dblclick(function(){
+    $(".image2").show("双击");
+});
+
+$(".image2").mouseenter(function(){
+    $(".text1").show("双击");
+});
+
+$(".image2").mouseleave(function(){
+    $(".text2").show("点击标题");
+});
+
+$(".title").click(function(){
+    $("div").hide("");
+    $(".image3").show("");
+    $(".text3").show("");
+});
+
